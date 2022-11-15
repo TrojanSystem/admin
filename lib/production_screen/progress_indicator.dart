2b7e067871e14e70c28f95f21b52486b2e59c7fd
TrojanSystem <suretion747@gmail.com>
2022-11-15 09:21:48 +0300

    Progress error

--- lib/production_screen/progress_indicator.dart
+++ lib/production_screen/progress_indicator.dart
@@ -7,10 +7,10 @@ class ProgressContainerItem extends StatefulWidget {
   final String soldItem;
   final String dailyProducedItem;
 
-  const ProgressContainerItem({
+  const ProgressContainerItem({Key key,
     this.soldItem,
     this.dailyProducedItem,
-  });
+  }) : super(key: key);
 
   @override
   State<ProgressContainerItem> createState() => _ProgressContainerItemState();
@@ -26,18 +26,26 @@ class _ProgressContainerItemState extends State<ProgressContainerItem> {
         int.parse(widget.soldItem);
     final percent = Provider.of<ProductionModelData>(context).percent();
     final showPercent =
-        Provider.of<ProductionModelData>(context).doublePercent();
+    Provider.of<ProductionModelData>(context).doublePercent();
 
     return Container(
       margin: const EdgeInsets.only(left: 10, right: 5, bottom: 5),
       padding: const EdgeInsets.only(left: 20, right: 20),
-      height: 90,
-      child: Row(
-        children: [
-          CircularPercentIndicator(
+
+
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey, width: 2),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),child: Row(
+
+      children: [
+        Container(margin: const EdgeInsets.only(bottom: 2),
+          child: CircularPercentIndicator(
             circularStrokeCap: CircularStrokeCap.round,
-            radius: 60.0,
-            lineWidth: 9.0,
+            radius: 32,
+            lineWidth: 8.0,
             percent: double.parse(percent),
             center: Text(
               '$showPercent %',
@@ -46,69 +54,64 @@ class _ProgressContainerItemState extends State<ProgressContainerItem> {
             progressColor: showPercent == 0
                 ? Colors.blueGrey[200]
                 : showPercent > 1 && showPercent < 25
-                    ? Colors.blue[900]
-                    : showPercent > 25 && showPercent < 50
-                        ? Colors.blue
-                        : showPercent > 50 && showPercent < 75
-                            ? Colors.blueAccent
-                            : showPercent > 75
-                                ? Colors.green
-                                : Colors.red,
+                ? Colors.blue[900]
+                : showPercent > 25 && showPercent < 50
+                ? Colors.blue
+                : showPercent > 50 && showPercent < 75
+                ? Colors.blueAccent
+                : showPercent > 75
+                ? Colors.green
+                : Colors.red,
           ),
-          const SizedBox(
-            width: 25,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Daily Progress',
-                style: TextStyle(
-                  fontWeight: FontWeight.w900,
-                  fontSize: 18,
-                ),
-              ),
-              RichText(
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: widget.soldItem.toString(),
-                      style: TextStyle(
-                        color: Colors.blue[900],
-                      ),
-                    ),
-                    TextSpan(
-                      text: ' / ',
-                      style: TextStyle(
-                        color: Colors.blue[900],
-                      ),
-                    ),
-                    TextSpan(
-                      text: widget.dailyProducedItem.toString(),
-                      style: TextStyle(
-                        color: Colors.blue[900],
-                      ),
-                    ),
-                    const TextSpan(
-                      text: ' Piece done',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey, width: 2),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(20),
         ),
-      ),
+        const SizedBox(
+          width: 25,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Daily Progress',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 18,
+              ),
+            ),
+            RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: widget.soldItem.toString(),
+                    style: TextStyle(
+                      color: Colors.blue[900],
+                    ),
+                  ),
+                  TextSpan(
+                    text: ' / ',
+                    style: TextStyle(
+                      color: Colors.blue[900],
+                    ),
+                  ),
+                  TextSpan(
+                    text: widget.dailyProducedItem.toString(),
+                    style: TextStyle(
+                      color: Colors.blue[900],
+                    ),
+                  ),
+                  const TextSpan(
+                    text: ' Piece done',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
     );
   }
 }
